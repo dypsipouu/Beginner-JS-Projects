4a6e00667e9d1d727a9d71707b86d2602b7d77d4
Indra Dipanegara Tan <117795391+dypsipouu@users.noreply.github.com>
2023-10-26 20:43:47 +0700

    Add files via upload
    A game where the user guesses a number 0-100

---
+++ numguesser.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync")();
+
+const target = Math.round(Math.random() * 100);
+
+let guesses = 0;
+
+while(true){
+    guesses++;
+
+    const guess = Number(prompt("Guess the number (0-100): "))
+    if (guess > target){
+        console.log("Your guess is too high")
+    } else if (guess < target){
+        console.log("Your guess is too low")
+    } else {
+        console.log("Your guess is correct!")
+        break;
+    }
+}
+console.log("You guessed the number in", guesses, "tries!");
